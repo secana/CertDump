f2b52c930c143529a6e8cd9032170f3b97bc6155
Stefan Hausotte <stefan.hausotte@gdata.de>
2018-12-19 14:55:44 +0100

    Initial commit

---
+++ CertDump/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Common;
+using System.IO;
+using CommandLine;
+using PeNet;
+
+namespace CertDump
+{
+    static class Program
+    {
+        public class Options
+        {
+            [Option('f', "file", Required = true, HelpText = "Input file to dump the certificate from.")]
+            public string File { get; set; }
+
+            [Option('o', "out", Required = false, Default = "cert.dump", HelpText = "Output file to dump the certificate to.")]
+            public string Out { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    DumpCert(o.File, o.Out);
+                });
+        }
+
+        static void DumpCert(string file, string output)
+        {
+            var peFile = new PeFile(file);
+            File.WriteAllBytes(output, peFile.WinCertificate.bCertificate);
+        }
+    }
+}
